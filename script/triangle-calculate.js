c1c1702dc6817ffb3b38f929a32d307922c953ef
mynul1111 <islamrhat12@gmail.com>
2024-02-15 19:40:19 +0600

    first commit

---
+++ script/triangle-calculate.js
@@ -0,0 +1,31 @@
+/**
+ * Objective: get base..................
+ * step-1: get base valu..........
+ * step-2: added an id......
+ * step-3: use getElementById............
+ * step-4: get value from.......
+ * step-5: consvert the value.......
+ */
+
+
+function calculateTriangleArea(){
+    // get triangle base value
+    const triangleBaseInput = document.getElementById('triangle-base');
+    const triangleBaseText = triangleBaseInput.value;
+    const base = parseFloat(triangleBaseText);
+    console.log(base);
+
+    // get triangle height value
+    const triangleHeightInput = document.getElementById('triangle-height');
+    const triangleHeightText = triangleHeightInput.value;
+    const height = parseFloat(triangleHeightText);
+    console.log(height);
+
+    // calculate triangle area
+    const area = 0.5 * base * height;
+    console.log('area of the triangle is:', area);
+
+    // display triangle area
+    const triangleAreaSpan = document.getElementById('triangle-area');
+    triangleAreaSpan.innerText = area;
+}
